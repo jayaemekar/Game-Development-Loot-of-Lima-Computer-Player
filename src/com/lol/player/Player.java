f6495144539e043c6fd4c06a7b8ebf96680ab992
Jaya Emekar <37955495+jayaemekar@users.noreply.github.com>
2021-03-31 07:18:37 +0530

    Update Player.java
    updated input as args

--- src/com/lol/player/Player.java
+++ src/com/lol/player/Player.java
@@ -1,7 +1,5 @@
 package com.lol.player;
 
-import java.util.Scanner;
-
 import com.lol.helper.PlayerInformation;
 import com.lol.helper.Utility;
 
@@ -12,24 +10,24 @@ public class Player {
 	 * @param args
 	 * @throws Exception
 	 */
-	@SuppressWarnings("resource")
+	
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
+	
 		System.out.println("\nEnter Your Team Name (in the format alpha, all etc.) : ");
-		String teamName = sc.nextLine();
+		String teamName = args[0]; 
 		
 		System.out.println("________________ LOOT OF LIMA!! Let's Begin the Game !_________________ ");
 		System.out.println("________________ Instructions to Play Game_____________________________ ");
 		System.out.println("1. All inputs should be given in CAPS letter");
 		System.out.println("2. Players are named as P1,P2,P3 etc.");
-		System.out.println("________________ Game Ready !! ________________________________________ ");
-				
+		System.out.println("________________ Game Ready !! ________________________________________ ");			
 		System.out.println("\nEnter Your Player Name (in the format P1,P2,P3 etc.) : ");
-		String playerNumber = sc.nextLine();
+		
+		String playerNumber = args[1]; 
 		
 		String fileWritePath = "/tmp/" + teamName +"from"+ playerNumber; // Write File path
 		String fileReadPath  = "/tmp/" + teamName +"to"  + playerNumber; // Read File path
-	
+		
 		PlayerInformation.getInstance().setFileReadPath(fileReadPath);
 		PlayerInformation.getInstance().setFileWritePath(fileWritePath);
 		PlayerInformation.getInstance().setGameOver(false); // Game over flag to set
