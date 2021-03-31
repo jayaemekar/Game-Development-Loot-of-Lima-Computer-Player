78406920c7b7d935b65f1ccb882ff5f8f1026aa5
Jaya Emekar <37955495+jayaemekar@users.noreply.github.com>
2021-03-31 11:41:41 +0530

    Update GameProcessing.java
    Removed random create question logic

--- src/com/lol/player/GameProcessing.java
+++ src/com/lol/player/GameProcessing.java
@@ -1,8 +1,6 @@
 package com.lol.player;
 
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 import java.util.Set;
 
 import com.lol.computer.player.ComputerPlayer;
@@ -21,8 +19,8 @@ public class GameProcessing {
 
 	/**
 	 * This method is to parse the dice information and start the Game play
-	 * 04:P1,SEM,NWB,SWF
-	 * This Dice Rolled message P1-Dice rolled for Player,
+	 * 04:P1,SEM,NWB,SWF This Dice Rolled message P1-Dice rolled for Player,
+	 * 
 	 * @param messageNumber
 	 * @param messageDetailsList
 	 */
@@ -41,7 +39,6 @@ public class GameProcessing {
 		if (!treasureGuessSent) {
 			istreasureLocFound = ComputerPlayerDeductionLogic.checkIsTreasureLocFound();
 			System.out.println("\nGuess the Treasure Locations???(YES/NO)???  :: " + istreasureLocFound);
-
 		}
 
 		if (Constants.YES.equalsIgnoreCase(istreasureLocFound))
@@ -49,137 +46,12 @@ public class GameProcessing {
 		else {
 			System.out.println(
 					"Message [" + messageNumber + "] Player " + playerName + " rolled the dices " + messageDetailsList);
-			if (PlayerInformation.getInstance().getPlayerName().equals(playerName) && !treasureGuessSent) {
-				
-				//createQuestion(messageDetailsList);
-				
+			if (PlayerInformation.getInstance().getPlayerName().equals(playerName) && !treasureGuessSent)
 				QuestionDeductionLogic.createQuestion(messageDetailsList);
-			} else if (treasureGuessSent) {
+			else if (treasureGuessSent)
 				System.out.println("Ignoring rolled the dices... Treasure guess sent already..!! waiting for reply...");
-			}
+
 		}
-	}
-
-	/**
-	 * This method is to build the question form human user input
-	 * 05:NNF,NEF,A,P3
-	 * 
-	 * @param messageDetailsList
-	 */
-
-	public void createQuestion(List<String> messageDetailsList) {
-		System.out.println("Select Two Die Faces...... ");
-		StringBuilder message = new StringBuilder(); //NN W,WW W,WW E
-		message.append("05:");
-
-		System.out.println("Enter DIE Face 1 :" + messageDetailsList);
-		String dieFace1 = messageDetailsList.get(new Random().nextInt(messageDetailsList.size()));
-		System.out.println("Selected DIE Face 1 :" + dieFace1);
-		if (PlayerInfoValidation.getInstance().validateDiceFace(dieFace1) && messageDetailsList.contains(dieFace1)) {
-			message.append(dieFace1).append(Constants.COMMA);
-		} else {
-			System.out.println("\n ====== Invalid DIE Face! Create question again ====== \n");
-			createQuestion(messageDetailsList);
-			return;
-		}
-
-		System.out.println("Enter DIE Face 2 :" + messageDetailsList);
-
-		String dieFace2 = messageDetailsList.get(new Random().nextInt(messageDetailsList.size()));
-		System.out.println("Selected DIE Face 2 :" + dieFace2);
-		if (PlayerInfoValidation.getInstance().validateDiceFace(dieFace2) && messageDetailsList.contains(dieFace2)
-				&& !dieFace2.equals(dieFace1)) {
-			message.append(dieFace2).append(Constants.COMMA);
-		} else {
-			System.out.println(" ====== Invalid DIE Face! Create question again ====== \n");
-			createQuestion(messageDetailsList);
-			return;
-		}
-
-		String dieFace1Terrian = String.valueOf(dieFace1.charAt(2));
-		String dieFace2Terrian = String.valueOf(dieFace2.charAt(2));
-
-		if (Constants.FOREST_CHAR.equals(dieFace1Terrian) && Constants.FOREST_CHAR.equals(dieFace2Terrian)) {
-			message.append(Constants.FOREST_CHAR).append(Constants.COMMA);
-		} else if (Constants.BEACH_CHAR.equals(dieFace1Terrian) && Constants.BEACH_CHAR.equals(dieFace2Terrian)) {
-			message.append(Constants.BEACH_CHAR).append(Constants.COMMA);
-		} else if (Constants.MOUNTAINS_CHAR.equals(dieFace1Terrian)
-				&& Constants.MOUNTAINS_CHAR.equals(dieFace2Terrian)) {
-			message.append(Constants.MOUNTAINS_CHAR).append(Constants.COMMA);
-		} else if (Constants.WILD_CHAR.equals(dieFace1Terrian) && Constants.WILD_CHAR.equals(dieFace2Terrian)) {
-			System.out.println("Select Area Token: M-Mountains, F-Forest, B-Beach, A-all ");
-			String[] areaTokenArr = { "A", "B", "F", "M" };
-			String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-			System.out.println("Selected Area Terrian ::" + terrianToken);
-			if (PlayerInfoValidation.getInstance().validateTerrianName(terrianToken))
-				message.append(terrianToken).append(Constants.COMMA);
-			else {
-				System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
-				createQuestion(messageDetailsList);
-				return;
-			}
-		} else if (Constants.WILD_CHAR.equals(dieFace1Terrian) || Constants.WILD_CHAR.equals(dieFace2Terrian)) {
-
-			if (Constants.FOREST_CHAR.equals(dieFace1Terrian) || Constants.FOREST_CHAR.equals(dieFace2Terrian)) {
-				System.out.println("Select Area Token: A-all or " + Constants.FOREST_CHAR + "- Forest only");
-				String[] areaTokenArr = { "A", "F" };
-				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-				System.out.println("Selected Area Terrian ::" + terrianToken);
-				if (Constants.FOREST_CHAR.equals(terrianToken) || Constants.ALL_CHAR.equals(terrianToken))
-					message.append(terrianToken).append(Constants.COMMA);
-				else {
-					System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
-					createQuestion(messageDetailsList);
-					return;
-				}
-			} else if (Constants.BEACH_CHAR.equals(dieFace1Terrian) || Constants.BEACH_CHAR.equals(dieFace2Terrian)) {
-				System.out.println("Select Area Token: A-all or " + Constants.BEACH_CHAR + "- Beach only");
-				String[] areaTokenArr = { "A", "B" };
-				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-				System.out.println("Selected Area Terrian ::" + terrianToken);
-				if (Constants.BEACH_CHAR.equals(terrianToken) || Constants.ALL_CHAR.equals(terrianToken))
-					message.append(terrianToken).append(Constants.COMMA);
-				else {
-					System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
-					createQuestion(messageDetailsList);
-					return;
-				}
-
-			} else if (Constants.MOUNTAINS_CHAR.equals(dieFace1Terrian)
-					|| Constants.MOUNTAINS_CHAR.equals(dieFace2Terrian)) {
-				System.out.println("Select Area Token: A-all or " + Constants.MOUNTAINS_CHAR + "- Mountain only");
-				String[] areaTokenArr = { "A", "M" };
-				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-				System.out.println("Selected Area Terrian ::" + terrianToken);
-				if (Constants.MOUNTAINS_CHAR.equals(terrianToken) || Constants.ALL_CHAR.equals(terrianToken))
-					message.append(terrianToken).append(Constants.COMMA);
-				else {
-					System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
-					createQuestion(messageDetailsList);
-					return;
-				}
-			}
-
-		} else {
-			message.append(Constants.ALL_CHAR).append(Constants.COMMA);
-		}
-
-		System.out.println("Enter Player number whom you want to ask the question :");
-
-		String playerAnswering = PlayerInformation.getInstance().getPlayerNameList()
-				.get(new Random().nextInt(PlayerInformation.getInstance().getPlayerNameList().size()));
-		System.out.println("Selected Player ::" + playerAnswering);
-		if (PlayerInfoValidation.getInstance().validatePlayerName(playerAnswering)
-				&& !playerAnswering.equalsIgnoreCase(PlayerInformation.getInstance().getPlayerName())) {
-			message.append(playerAnswering);
-		} else {
-			System.out.println(" ====== Invalid Player! Create question again ====== \n");
-			createQuestion(messageDetailsList);
-			return;
-		}
-
-		Utility.writeFile(PlayerInformation.getInstance().getFileWritePath(), message.toString());
-		Utility.parseMessage(message.toString());
 	}
 
 	/**
@@ -190,46 +62,10 @@ public class GameProcessing {
 	 */
 	public void getQuestionInformation(String messageNumber, List<String> messageDetailsList) {
 
-		String playerName = messageDetailsList.get(3);
-		if (PlayerInformation.getInstance().getPlayerName().equals(playerName)) {
-			System.out.println(" ====== Question is asked to you ====== ");
-			createAnswerInformationMsg(messageDetailsList);
-		} else {
-			System.out.println("\nMessage [" + messageNumber + "] Question asked to player " + messageDetailsList.get(3)
-					+ ", How many "
-					+ PlayerInformation.getInstance().getTerrianTokenInformation(messageDetailsList.get(2))
-					+ " terrains between " + messageDetailsList.get(0) + " and " + messageDetailsList.get(1));
-		}
-	}
+		System.out.println("\nMessage [" + messageNumber + "] Question asked to player " + messageDetailsList.get(3)
+				+ ", How many " + PlayerInformation.getInstance().getTerrianTokenInformation(messageDetailsList.get(2))
+				+ " terrains between " + messageDetailsList.get(0) + " and " + messageDetailsList.get(1));
 
-	@SuppressWarnings("resource")
-	private void createAnswerInformationMsg(List<String> messageDetailsList) {
-
-		// Message [06] Player P3 has 2 token in Forest terrain between North
-		// and North East
-		// 05:NNF,NEF,A,P1
-		// 06:NNF,NEF,F,2,P1,P3
-		System.out.println(
-				"How many " + PlayerInformation.getInstance().getTerrianTokenInformation(messageDetailsList.get(2))
-						+ " terrain between "
-						+ PlayerInformation.getInstance().getDirectionInformation(messageDetailsList.get(0)) + " "
-						+ PlayerInformation.getInstance().getTerrianTokenInformation(messageDetailsList.get(0))
-						+ " and " + PlayerInformation.getInstance().getDirectionInformation(messageDetailsList.get(1))
-						+ " " + PlayerInformation.getInstance().getTerrianTokenInformation(messageDetailsList.get(1)));
-
-		StringBuilder message = new StringBuilder();
-		message.append("06:").append(messageDetailsList.get(0)).append(Constants.COMMA)
-				.append(messageDetailsList.get(1)).append(Constants.COMMA).append(messageDetailsList.get(2))
-				.append(Constants.COMMA);
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the total number of terrian you have in the selected location :");
-		String terrianCount = sc.nextLine();
-		message.append(terrianCount).append(Constants.COMMA).append(PlayerInformation.getInstance().getPlayerName())
-				.append(Constants.COMMA).append(messageDetailsList.get(3));
-
-		Utility.writeFile(PlayerInformation.getInstance().getFileWritePath(), message.toString());
-		Utility.parseMessage(message.toString());
 	}
 
 	/**
@@ -252,6 +88,7 @@ public class GameProcessing {
 	/**
 	 * This method is to send the treasure Guess message to the server
 	 * 07:P1,3F,6M
+	 * 
 	 * @param messageNumber
 	 * @param messageDetailsList
 	 * @return
@@ -261,10 +98,10 @@ public class GameProcessing {
 		StringBuilder message = new StringBuilder("07:");
 		Set<String> treasureLoc = ComputerPlayer.getInstance().getTreasureLoc();
 		String[] treasureLocArr = treasureLoc.toArray(new String[treasureLoc.size()]);
-		
+
 		message.append(PlayerInformation.getInstance().getPlayerName()).append(Constants.COMMA);
 		String guessOne = treasureLocArr[0];
-		System.out.println("First guess identified as :: " +guessOne);
+		System.out.println("First guess identified as :: " + guessOne);
 		if (PlayerInfoValidation.getInstance().validateTerrianLocation(guessOne)) {
 			message.append(guessOne).append(Constants.COMMA);
 		} else {
@@ -272,9 +109,9 @@ public class GameProcessing {
 			treasureGuess();
 			return false;
 		}
-		
+
 		String guessTwo = treasureLocArr[1];
-		System.out.println("Second guess identified as :: "+guessTwo);
+		System.out.println("Second guess identified as :: " + guessTwo);
 		if (PlayerInfoValidation.getInstance().validateTerrianLocation(guessTwo)) {
 			message.append(guessTwo);
 		} else {
@@ -297,6 +134,7 @@ public class GameProcessing {
 	/**
 	 * This method is used to parse the winner player and token information
 	 * 08:player1,wintoken1,wintoken2 eg.08:P1,3F,6M
+	 * 
 	 * @param messageNumber
 	 * @param messageDetailsList
 	 */
@@ -317,6 +155,7 @@ public class GameProcessing {
 
 	/**
 	 * treasure guess was incorrect and disqualify 09:P2
+	 * 
 	 * @param messageNumber
 	 * @param messageDetailsList
 	 * @return
