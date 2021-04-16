b13378e45191a95689838d9dcd9f20275a572057
Jaya Emekar <37955495+jayaemekar@users.noreply.github.com>
2021-04-15 23:24:23 -0600

    Update QuestionDeductionLogic.java

--- src/com/lol/computer/player/QuestionDeductionLogic.java
+++ src/com/lol/computer/player/QuestionDeductionLogic.java
@@ -3,6 +3,7 @@ package com.lol.computer.player;
 import static java.util.Map.Entry.comparingByValue;
 import static java.util.stream.Collectors.toMap;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -19,6 +20,11 @@ import com.lol.helper.PlayerInformation;
 import com.lol.helper.Utility;
 import com.lol.validation.PlayerInfoValidation;
 
+/**
+ * This class is used to deduce the question logic
+ * 
+ *
+ */
 public class QuestionDeductionLogic {
 
 	private static Boolean SetShovel = true;
@@ -32,7 +38,7 @@ public class QuestionDeductionLogic {
 	 * @param messageDetailsList
 	 */
 
-	public static void pistol() {
+	private static void pistol() {
 
 		HashSet<String> defaultMap = new HashSet<String>(ComputerPlayer.getInstance().getAllPlayerTrrianMap().keySet());
 		HashSet<String> nonTreasureLocation = new HashSet<String>(ComputerPlayer.getInstance().getNotTreasureLoc());
@@ -61,6 +67,11 @@ public class QuestionDeductionLogic {
 		Utility.parseMessage(message);
 	}
 
+	/**
+	 * This method is used to create the question intelligently
+	 * 
+	 * @param messageDetailsList
+	 */
 	public static void createQuestion(List<String> messageDetailsList) {
 
 		if (ComputerPlayer.getInstance().getNotTreasureLoc().size() >= 20 && !SetPistol) {
@@ -87,7 +98,6 @@ public class QuestionDeductionLogic {
 			messageMap = messageMapAll;
 		Map<String, Integer> sortedMapByValue = messageMap.entrySet().stream().sorted(comparingByValue())
 				.collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
-		System.out.println("sortedMapByValue  :: " + sortedMapByValue);
 
 		if (!sortedMapByValue.isEmpty()) {
 			for (Entry<String, Integer> en : sortedMapByValue.entrySet()) {
@@ -99,9 +109,8 @@ public class QuestionDeductionLogic {
 						System.out.println("Selected  Die Face  Two :" + message.substring(7, 10));
 						System.out.println("Selected  Area  Terrain  :" + message.substring(11, 12));
 						System.out.println("Selected  Player :" + message.substring(13, 15));
-
 						System.out.println("Message Type : " + message.substring(16));
-						if (message.substring(16).equals("S")) {
+						if (message.substring(16).equals(Constants.S_CHAR)) {
 							SetShovel = false;
 							ComputerPlayer.getInstance().setShovelFlag();
 						}
@@ -109,7 +118,6 @@ public class QuestionDeductionLogic {
 						Utility.parseMessage(message);
 						break;
 					} else if (en.getValue() > 3 && ComputerPlayer.getInstance().getBarrelFlagStatus() == false) {
-
 						Barrel = true;
 						String RerollMessage = RerollDieReq(2, message.substring(3, 6), message.substring(7, 10),
 								messageDetailsList);
@@ -127,6 +135,7 @@ public class QuestionDeductionLogic {
 	}
 
 	/**
+	 * This method is used to generate question Map
 	 * 
 	 * @param dieFaceOne
 	 * @param dieFaceTwo
@@ -135,31 +144,34 @@ public class QuestionDeductionLogic {
 	 * @param msgType
 	 * @return
 	 */
-	public static HashMap<String, HashMap<String, Integer>> generateQuestionMap(String dieFaceOne, String dieFaceTwo,
+	private static HashMap<String, HashMap<String, Integer>> generateQuestionMap(String dieFaceOne, String dieFaceTwo,
 			String terrainType, SortedMap<String, Integer> messageMap, SortedMap<String, Integer> messageMapAll,
 			String msgType) {
 
-		System.out.println("messageMap####:" + messageMap);
+		String directionOne = String.valueOf(dieFaceOne.charAt(0)) + String.valueOf(dieFaceOne.charAt(1));
+		String directionTwo = String.valueOf(dieFaceTwo.charAt(0)) + String.valueOf(dieFaceTwo.charAt(1));
+		ComputerPlayer.getInstance().setHead(ComputerPlayer.getInstance().createNode(directionOne));
+		ComputerPlayer.getInstance().setTail(ComputerPlayer.getInstance().createNode(directionTwo));
 		Node current = ComputerPlayer.getInstance().getHead();
 		HashMap<String, HashMap<String, Integer>> terrainCountMap = new HashMap<>();
+		// counting the location between two directions
 		do {
 
 			current.terrainList.entrySet().stream().forEach(terrianMap -> {
 
 				terrianMap.getValue().entrySet().stream().forEach(playerMap -> {
 
-					if (!playerMap.getKey().equals(PlayerInformation.getInstance().getPlayerName())
-							&& playerMap.getValue() == -1) {
-
+					if (playerMap.getValue() == Constants.TENTATIVE_TERRAIN) {
+						String terrainChar = terrianMap.getKey().substring(1, 2);
 						if (!terrainCountMap.containsKey(playerMap.getKey()))
 							terrainCountMap.put(playerMap.getKey(), new HashMap<String, Integer>());
 
-						if (!terrainCountMap.get(playerMap.getKey()).containsKey(terrianMap.getKey().substring(1, 2)))
-							terrainCountMap.get(playerMap.getKey()).put(terrianMap.getKey().substring(1, 2), 0);
+						if (!terrainCountMap.get(playerMap.getKey()).containsKey(terrainChar))
+							terrainCountMap.get(playerMap.getKey()).put(terrainChar, 0);
 
-						int count = terrainCountMap.get(playerMap.getKey()).get(terrianMap.getKey().substring(1, 2));
-						count = count + 1;
-						terrainCountMap.get(playerMap.getKey()).replace(terrianMap.getKey().substring(1, 2), count);
+						int count = terrainCountMap.get(playerMap.getKey()).get(terrainChar);
+
+						terrainCountMap.get(playerMap.getKey()).replace(terrainChar, ++count);
 
 					}
 				});
@@ -171,25 +183,27 @@ public class QuestionDeductionLogic {
 
 		HashSet<String> terrainTypes = new HashSet<String>();
 		if (Constants.ALL_CHAR.equals(terrainType)) {
-			terrainTypes.add("M");
-			terrainTypes.add("F");
-			terrainTypes.add("B");
+			terrainTypes.add(Constants.MOUNTAINS_CHAR);
+			terrainTypes.add(Constants.FOREST_CHAR);
+			terrainTypes.add(Constants.BEACH_CHAR);
 		} else {
 			terrainTypes.add(terrainType);
 		}
 
+		// creating valid question messages
 		terrainCountMap.entrySet().forEach(player -> {
 			player.getValue().entrySet().stream().forEach(terrain -> {
 				Integer terrainCount = 24;
 				if (terrain.getValue() <= terrainCount && terrainTypes.contains(terrain.getKey())) {
 
-					String message = "05:" + dieFaceOne + "," + dieFaceTwo + "," + terrainType + "," + player.getKey();
+					String message = Constants.MESSAAGE_05 + dieFaceOne + "," + dieFaceTwo + "," + terrainType + ","
+							+ player.getKey();
 
-					if (msgType.equals("S")) {
-						message = message.concat(",S");
+					if (msgType.equals(Constants.S_CHAR)) {
+						message = message.concat("," + Constants.S_CHAR);
 						ComputerPlayer.getInstance().setShovelFlag(); // shovel used marked
-					} else if (msgType.equals("Q")) {
-						message = message.concat(",Q"); // default message type
+					} else if (msgType.equals(Constants.Q_CHAR)) {
+						message = message.concat("," + Constants.Q_CHAR); // default message type
 					}
 					if (!dieFaceOne.substring(2).equals(dieFaceTwo.substring(2)))
 						if (!Constants.ALL_CHAR.equals(terrainType))
@@ -205,57 +219,54 @@ public class QuestionDeductionLogic {
 	}
 
 	/**
+	 * Creating question based on the direction
 	 * 
 	 * @param dieFaceOne
 	 * @param dieFaceTwo
 	 * @param messageMap
 	 */
-	public static void questionProxy(String dieFaceOne, String dieFaceTwo, SortedMap<String, Integer> messageMap,
+	private static void questionProxy(String dieFaceOne, String dieFaceTwo, SortedMap<String, Integer> messageMap,
 			SortedMap<String, Integer> messageMapAll) {
 
-		String directionOne = String.valueOf(dieFaceOne.charAt(0)) + String.valueOf(dieFaceOne.charAt(1));
-		String directionTwo = String.valueOf(dieFaceTwo.charAt(0)) + String.valueOf(dieFaceTwo.charAt(1));
 		String headTerrian = String.valueOf(dieFaceOne.charAt(2));
 		String tailTerrian = String.valueOf(dieFaceTwo.charAt(2));
 
-		ComputerPlayer.getInstance().setHead(ComputerPlayer.getInstance().createNode(directionOne));
-		ComputerPlayer.getInstance().setTail(ComputerPlayer.getInstance().createNode(directionTwo));
-
-		HashSet<String> terrains = new HashSet<String>();
-		terrains.add("M");
-		terrains.add("B");
-		terrains.add("F");
+		HashSet<String> terrains = new HashSet<String>(
+				Arrays.asList(Constants.MOUNTAINS_CHAR, Constants.BEACH_CHAR, Constants.FOREST_CHAR));
 
 		if (headTerrian.equals(tailTerrian) && !Constants.WILD_CHAR.equals(headTerrian)) {
-			generateQuestionMap(dieFaceOne, dieFaceTwo, tailTerrian, messageMap, messageMapAll, "Q");
+			generateQuestionMap(dieFaceOne, dieFaceTwo, tailTerrian, messageMap, messageMapAll, Constants.Q_CHAR);
 			if (SetShovel && ComputerPlayer.getInstance().getRoundCount() > 10) {
 				terrains.remove(dieFaceOne.substring(2, 3));
 				dieFaceOne = dieFaceOne.substring(0, 2)
 						+ terrains.stream().skip(new Random().nextInt(terrains.size())).findFirst().orElse(null);
-				generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.ALL_CHAR, messageMap, messageMapAll, "S");
+				generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.ALL_CHAR, messageMap, messageMapAll,
+						Constants.S_CHAR);
 			}
 
 		} else if (!headTerrian.equals(tailTerrian)
 				&& !(Constants.WILD_CHAR.equals(headTerrian) || Constants.WILD_CHAR.equals(tailTerrian))) {
-			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.ALL_CHAR, messageMap, messageMapAll, "Q");
+			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.ALL_CHAR, messageMap, messageMapAll,
+					Constants.Q_CHAR);
 			if (SetShovel && ComputerPlayer.getInstance().getRoundCount() > 10) {
 				String temp = dieFaceTwo.substring(2, 3);
 				terrains.remove(temp);
 				String dieFaceTwo2 = dieFaceTwo.subSequence(0, 2) + headTerrian;
-				generateQuestionMap(dieFaceOne, dieFaceTwo2, headTerrian, messageMap, messageMapAll, "S");
+				generateQuestionMap(dieFaceOne, dieFaceTwo2, headTerrian, messageMap, messageMapAll, Constants.S_CHAR);
 				terrains.add(temp);
 				temp = dieFaceOne.substring(2, 3);
 				terrains.remove(temp);
 				String dieFaceOne1 = dieFaceOne.substring(0, 2) + tailTerrian;
-				generateQuestionMap(dieFaceOne1, dieFaceTwo, tailTerrian, messageMap, messageMapAll, "S");
+				generateQuestionMap(dieFaceOne1, dieFaceTwo, tailTerrian, messageMap, messageMapAll, Constants.S_CHAR);
 
 			}
 
 		} else if (!headTerrian.equals(tailTerrian)
 				&& (Constants.WILD_CHAR.equals(headTerrian) || Constants.WILD_CHAR.equals(tailTerrian))) {
 			String terrainType = !Constants.WILD_CHAR.equals(headTerrian) ? headTerrian : tailTerrian;
-			generateQuestionMap(dieFaceOne, dieFaceTwo, terrainType, messageMap, messageMapAll, "Q");
-			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.ALL_CHAR, messageMap, messageMapAll, "Q");
+			generateQuestionMap(dieFaceOne, dieFaceTwo, terrainType, messageMap, messageMapAll, Constants.Q_CHAR);
+			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.ALL_CHAR, messageMap, messageMapAll,
+					Constants.Q_CHAR);
 			if (SetShovel && ComputerPlayer.getInstance().getRoundCount() > 10) {
 				terrains.remove(terrainType);
 				Iterator<String> terrainIterator = terrains.iterator();
@@ -272,37 +283,36 @@ public class QuestionDeductionLogic {
 				}
 			}
 		} else {
-			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.MOUNTAINS_CHAR, messageMap, messageMapAll, "Q");
-			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.BEACH_CHAR, messageMap, messageMapAll, "Q");
-			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.FOREST_CHAR, messageMap, messageMapAll, "Q");
+			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.MOUNTAINS_CHAR, messageMap, messageMapAll,
+					Constants.Q_CHAR);
+			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.BEACH_CHAR, messageMap, messageMapAll,
+					Constants.Q_CHAR);
+			generateQuestionMap(dieFaceOne, dieFaceTwo, Constants.FOREST_CHAR, messageMap, messageMapAll,
+					Constants.Q_CHAR);
 		}
 	}
 
+	/**
+	 * Create question randomly in case any exception
+	 * 
+	 * @param messageDetailsList
+	 */
 	private static void createQuestionRandomly(List<String> messageDetailsList) {
-		System.out.println("Select Two Die Faces...... ");
-		StringBuilder message = new StringBuilder(); // NN W,WW W,WW E
-		message.append("05:");
 
-		System.out.println("Enter DIE Face 1 :" + messageDetailsList);
+		StringBuilder message = new StringBuilder(); // NN W,WW W,WW E
+		message.append(Constants.MESSAAGE_05);
 		String dieFace1 = messageDetailsList.get(new Random().nextInt(messageDetailsList.size()));
-		System.out.println("Selected DIE Face 1 :" + dieFace1);
 		if (PlayerInfoValidation.getInstance().validateDiceFace(dieFace1) && messageDetailsList.contains(dieFace1)) {
 			message.append(dieFace1).append(Constants.COMMA);
 		} else {
-			System.out.println("\n ====== Invalid DIE Face! Create question again ====== \n");
 			createQuestionRandomly(messageDetailsList);
 			return;
 		}
-
-		System.out.println("Enter DIE Face 2 :" + messageDetailsList);
-
 		String dieFace2 = messageDetailsList.get(new Random().nextInt(messageDetailsList.size()));
-		System.out.println("Selected DIE Face 2 :" + dieFace2);
 		if (PlayerInfoValidation.getInstance().validateDiceFace(dieFace2) && messageDetailsList.contains(dieFace2)
 				&& !dieFace2.equals(dieFace1)) {
 			message.append(dieFace2).append(Constants.COMMA);
 		} else {
-			System.out.println(" ====== Invalid DIE Face! Create question again ====== \n");
 			createQuestionRandomly(messageDetailsList);
 			return;
 		}
@@ -318,54 +328,47 @@ public class QuestionDeductionLogic {
 				&& Constants.MOUNTAINS_CHAR.equals(dieFace2Terrian)) {
 			message.append(Constants.MOUNTAINS_CHAR).append(Constants.COMMA);
 		} else if (Constants.WILD_CHAR.equals(dieFace1Terrian) && Constants.WILD_CHAR.equals(dieFace2Terrian)) {
-			System.out.println("Select Area Token: M-Mountains, F-Forest, B-Beach, A-all ");
+
 			String[] areaTokenArr = { "A", "B", "F", "M" };
-			String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-			System.out.println("Selected Area Terrian ::" + terrianToken);
+			String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];
+
 			if (PlayerInfoValidation.getInstance().validateTerrianName(terrianToken))
 				message.append(terrianToken).append(Constants.COMMA);
 			else {
-				System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
 				createQuestionRandomly(messageDetailsList);
 				return;
 			}
 		} else if (Constants.WILD_CHAR.equals(dieFace1Terrian) || Constants.WILD_CHAR.equals(dieFace2Terrian)) {
 
 			if (Constants.FOREST_CHAR.equals(dieFace1Terrian) || Constants.FOREST_CHAR.equals(dieFace2Terrian)) {
-				System.out.println("Select Area Token: A-all or " + Constants.FOREST_CHAR + "- Forest only");
+
 				String[] areaTokenArr = { "A", "F" };
-				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-				System.out.println("Selected Area Terrian ::" + terrianToken);
+				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];
 				if (Constants.FOREST_CHAR.equals(terrianToken) || Constants.ALL_CHAR.equals(terrianToken))
 					message.append(terrianToken).append(Constants.COMMA);
 				else {
-					System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
 					createQuestionRandomly(messageDetailsList);
 					return;
 				}
 			} else if (Constants.BEACH_CHAR.equals(dieFace1Terrian) || Constants.BEACH_CHAR.equals(dieFace2Terrian)) {
-				System.out.println("Select Area Token: A-all or " + Constants.BEACH_CHAR + "- Beach only");
+
 				String[] areaTokenArr = { "A", "B" };
-				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-				System.out.println("Selected Area Terrian ::" + terrianToken);
+				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];
 				if (Constants.BEACH_CHAR.equals(terrianToken) || Constants.ALL_CHAR.equals(terrianToken))
 					message.append(terrianToken).append(Constants.COMMA);
 				else {
-					System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
 					createQuestionRandomly(messageDetailsList);
 					return;
 				}
 
 			} else if (Constants.MOUNTAINS_CHAR.equals(dieFace1Terrian)
 					|| Constants.MOUNTAINS_CHAR.equals(dieFace2Terrian)) {
-				System.out.println("Select Area Token: A-all or " + Constants.MOUNTAINS_CHAR + "- Mountain only");
 				String[] areaTokenArr = { "A", "M" };
-				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];// "A";//sc.nextLine();
-				System.out.println("Selected Area Terrian ::" + terrianToken);
+				String terrianToken = areaTokenArr[new Random().nextInt(areaTokenArr.length)];
+
 				if (Constants.MOUNTAINS_CHAR.equals(terrianToken) || Constants.ALL_CHAR.equals(terrianToken))
 					message.append(terrianToken).append(Constants.COMMA);
 				else {
-					System.out.println(" ====== Invalid Terrian Token! Ask Question Again!! ====== \n");
 					createQuestionRandomly(messageDetailsList);
 					return;
 				}
@@ -375,25 +378,30 @@ public class QuestionDeductionLogic {
 			message.append(Constants.ALL_CHAR).append(Constants.COMMA);
 		}
 
-		System.out.println("Enter Player number whom you want to ask the question :");
-
 		String playerAnswering = PlayerInformation.getInstance().getPlayerNameList()
 				.get(new Random().nextInt(PlayerInformation.getInstance().getPlayerNameList().size()));
-		System.out.println("Selected Player ::" + playerAnswering);
+
 		if (PlayerInfoValidation.getInstance().validatePlayerName(playerAnswering)
 				&& !playerAnswering.equalsIgnoreCase(PlayerInformation.getInstance().getPlayerName())) {
 			message.append(playerAnswering);
 		} else {
-			System.out.println(" ====== Invalid Player! Create question again ====== \n");
 			createQuestionRandomly(messageDetailsList);
 			return;
 		}
-		message.append(",Q");
+		message.append("," + Constants.Q_CHAR);
 		Utility.writeFile(PlayerInformation.getInstance().getFileWritePath(), message.toString());
 		Utility.parseMessage(message.toString());
 	}
 
-	public static String RerollDieReq(int numberofDies, String die1, String die2, List<String> messageDetailsList) {
+	/**
+	 * This is used to create the Barrel message
+	 * @param numberofDies
+	 * @param die1
+	 * @param die2
+	 * @param messageDetailsList
+	 * @return
+	 */
+	private static String RerollDieReq(int numberofDies, String die1, String die2, List<String> messageDetailsList) {
 		return Constants.MESSAAGE_12 + numberofDies + "," + messageDetailsList.indexOf(die1) + ","
 				+ messageDetailsList.indexOf(die2);
 
